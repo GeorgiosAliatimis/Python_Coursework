9d63425526c9d3b71253f8815ca503e773ad2546
Aliatimis <aliatimi@lancaster.ac.uk>
2022-01-27 12:31:53 +0000

    score whisker plots

--- scores.py
+++ scores.py
@@ -1,6 +1,7 @@
 from stable_marriage import Stable_Marriage_Random_Tables
 import matplotlib.pyplot as plt
 import random
+from numpy import transpose, linspace
 
 def random_score(n):
     r = Stable_Marriage_Random_Tables(n)
@@ -24,5 +25,28 @@ def plot_hist(n, **kwargs):
     plt.savefig(f"scores_n={n}.png",format="png")
 
 random.seed(1)
-plot_hist(10)
-plot_hist(100,M=300)
+# plot_hist(10)
+# plot_hist(100,M=300)
+
+n_vals =  2**linspace(1,10,10)
+scores_men = [None] * len(n_vals)
+scores_women = [None] * len(n_vals)
+for i, n in enumerate(n_vals):
+    scores_men[i], scores_women[i] = random_scores(int(n),M=50)
+
+n_vals_str = [str(int(num)) for num in n_vals]
+ax = plt.subplot()
+for score,c in  [(scores_men,"blue"), (scores_women,"red")]:
+    ax.boxplot(transpose(score), boxprops=dict(color=c),
+                capprops=dict(color=c),
+                whiskerprops=dict(color=c),
+                flierprops=dict(color=c, markeredgecolor=c),
+                medianprops=dict(color=c),
+                positions = n_vals,
+                widths= n_vals/3
+                )
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.set_xlabel(r'n')
+ax.set_title('Score whisker plots')
+plt.savefig("scores_whisker_plot.png")
